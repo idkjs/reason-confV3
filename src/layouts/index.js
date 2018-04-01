957e05510973370ed3f2bc5e51b1b8b2f57b309b
Alain Armand <aarmand.inbox@gmail.com>
2018-04-01 09:55:08 +0200

    update helmut info

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,53 +6,15 @@ import Helmet from "react-helmet";
 
 import "./index.css";
 
-// const Header = () => (
-//   <div
-//     style={{
-//       background: "rebeccapurple",
-//       marginBottom: "1.45rem"
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: "0 auto",
-//         maxWidth: 960,
-//         padding: "1.45rem 1.0875rem"
-//       }}
-//     >
-//       <h1 style={{ margin: 0 }}>
-//         <Link
-//           to="/"
-//           style={{
-//             color: "white",
-//             textDecoration: "none"
-//           }}
-//         >
-//           Demo 2018
-//         </Link>
-//       </h1>
-//     </div>
-//   </div>
-// );
-
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="Demo 2018"
       meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
+        { name: "description", content: "Demo 2018" },
+        { name: "keywords", content: "Gatsby, Conference, Reason, Reason Conf" }
       ]}
     />
-    {/* <Header />
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "0px 1.0875rem 1.45rem",
-        paddingTop: 0
-      }}
-    > */}
     {children()}
   </div>
 );
