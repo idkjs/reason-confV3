cec15bcc9fdf646c156ac0639e6c8bc11cb46943
Alain Armand <aarmand.inbox@gmail.com>
2018-04-10 13:21:10 +0200

    removed public directory

--- public/path---coc-a0e39f21c11f6a62c5ab.js
+++
@@ -1,6 +0,0 @@
-webpackJsonp([0xec36e1e8a58e], {
-  402: function(e, t) {
-    e.exports = { pathContext: {} };
-  }
-});
-//# sourceMappingURL=path---coc-a0e39f21c11f6a62c5ab.js.map
--- public/path---imprint-a0e39f21c11f6a62c5ab.js
+++
@@ -1,6 +0,0 @@
-webpackJsonp([0x8e3cf1a8e51e], {
-  404: function(e, t) {
-    e.exports = { pathContext: {} };
-  }
-});
-//# sourceMappingURL=path---imprint-a0e39f21c11f6a62c5ab.js.map
